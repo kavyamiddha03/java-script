f728c1ea71b02a6a605d57d15eed49b416318aa4
kavyamiddha03 <143822115+kavyamiddha03@users.noreply.github.com>
2023-09-02 16:56:00 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 213456;
+let accountEmail = "kavya123@gmail.com"
+var accountPin = "212121"
+accountCity = "jaipur"
+let accountState;
+
+accountEmail = "h23bb@gmail.com"
+accountPin = "234"
+accountCity = "delhi"
+
+// accountId = 2;
+
+/* Prefer not to use var
+because it has issue of block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPin,accountCity, accountState])
