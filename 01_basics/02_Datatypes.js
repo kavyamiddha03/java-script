69f45086c716611238f025d003adac3ff088dc5d
kavyamiddha03 <143822115+kavyamiddha03@users.noreply.github.com>
2023-09-02 17:19:42 +0000

    Data types

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,27 @@
+"use strict" // Used to treat code as newer version
+
+// alert(3 + 3);   because we workig in node envrionment not in browser
+
+console.log("hello"); console.log("hy")
+
+// Code Readibilty should be high
+
+// Ecma script is real documentation for js 
+
+// Data types
+
+let age = 23;
+let name = "kavya";
+let isLOggedin = false;
+
+// number => 2 to the power 53 int is used
+// Bigint => used for no.greater then int
+//string => ""
+//boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => uniqueness
+
+//object
+
+console.log(typeof null);
