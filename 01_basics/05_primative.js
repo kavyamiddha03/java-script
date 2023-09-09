32316a1c1d1687aef599d37d6640caf539aa46df
kavyamiddha03 <143822115+kavyamiddha03@users.noreply.github.com>
2023-09-09 14:04:44 +0000

    primative

---
+++ 01_basics/05_primative.js
@@ -0,0 +1,34 @@
+// Primative Data types 
+// String , Number , boolean , Symbol , undifined , Null , BigInt
+// Javascript is a dynamically Typed Language because it assign datatypes on run time 
+
+const score = 100 
+const scorevalue = 100.3
+
+const isLoggedIn = false
+const OutsideTemp = null
+let userEmail;
+
+const id  = Symbol('123')
+const symbolId = Symbol('123')
+
+// console.log(id === symbolId); false
+
+const bigNumber = 932985809182489n
+
+// Reference (Non primative )
+// Array objects Functions
+
+const heros = ["harry", "hitesh", "jenny"]
+
+let Mylife = {
+    name: "kavya",
+    age: 23,
+    salary: 0
+}
+
+const myFunction = function(){
+
+    console.log("hello world");
+
+}
