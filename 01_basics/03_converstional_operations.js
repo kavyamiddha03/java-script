2affc439be86af3277b19895e64bd56232895b5b
kavyamiddha03 <143822115+kavyamiddha03@users.noreply.github.com>
2023-09-09 13:52:08 +0000

    Comparison

---
+++ 01_basics/03_converstional_operations.js
@@ -0,0 +1,45 @@
+let score= "hitesh"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInnumber = Number(score)
+// console.log(typeof valueInnumber)
+// console.log( valueInnumber)
+
+// 33=> 33 => number
+// "33abc" => NaN
+//" " => 0
+//true => 1
+//false => 0
+//"hitesh" => NaN
+
+let Isloggenin = ""
+
+let booleanLoggedIn = Boolean(Isloggenin)
+console.log( booleanLoggedIn);
+
+// 1 => true
+// 0 => false
+//"" => false
+// "hitesh" => true
+// "33" => true
+// 33 => true
+
+
+// *************************************** Operations ****************************************************************
+
+let value = 3 
+negvalue = -value
+
+console.log(negvalue)
+
+let str1 = "hello"
+let str2 = " kavya"
+str3 = str1 + str2;
+console.log(str3);
+
+console.log(1 + "2");
+console.log("1" + 2);
+console.log(1 + 2 + "2");
+console.log("1" + 2 + "2");
