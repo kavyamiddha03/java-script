f43a83e55822d45d80394cd7169e1b8b56592166
kavyamiddha03 <143822115+kavyamiddha03@users.noreply.github.com>
2023-09-13 13:41:04 +0000

    numbers

---
+++ 01_basics/08_Numbers.js
@@ -0,0 +1,34 @@
+// const score = 100;
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(3));
+
+// const Othernumber = 123.8966;
+// console.log(Othernumber.toPrecision(4));
+
+// const digit = 1000000
+// console.log(digit.toLocaleString('en-IN'));
+
+// ++++++++++++++++++++++++++++++++++  Maths +++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.abs(4)); // abs only change neg. value
+// console.log(Math.round(4.6));
+console.log(Math.ceil(4.6));
+console.log(Math.floor(4.6));
+console.log(Math.min(4,1,3,1,7));
+console.log(Math.max(4,1,3,1,7));
+
+// console.log(Math.random());
+// console.log((Math.random()*10) + 1);
+// console.log((Math.floor(Math.random()*10) + 1));
+
+const min = 1;
+const max = 6;
+console.log(Math.floor(Math.random() * ( max - min + 1) + min));
+
