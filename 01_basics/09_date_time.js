0504fc58b6e43d8e19f279cf24b2305d28a48033
kavyamiddha03 <143822115+kavyamiddha03@users.noreply.github.com>
2023-09-13 16:25:41 +0000

    arrays

---
+++ 01_basics/09_date_time.js
@@ -0,0 +1,35 @@
+//dates
+
+const myDAte = new Date();
+// console.log(myDAte.toString());
+// console.log(myDAte.toLocaleDateString());
+// console.log(myDAte.toLocaleString());
+// console.log(myDAte.toDateString());
+// console.log(myDAte.toJSON());
+
+// console.log(typeof myDAte);
+
+// let CreatedDate = new Date(2023, 1 , 4)
+// let CreatedDate = new Date(2023, 1 , 4, 5 , 30,)
+let CreatedDate = new Date("2023-01-05")
+// console.log(CreatedDate.toLocaleString());
+
+let Mytimestamp = Date.now();
+
+// console.log(Mytimestamp);
+// console.log(CreatedDate.getTime());
+
+console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate.getDate());
+console.log(newDate.getMonth());
+console.log(newDate.getHours());
+console.log(newDate.toLocaleString());
+console.log(newDate.toDateString());
+
+// `${newDate.getdate()} and the time is`
+
+newDate.toDateString('default',{
+    weekday: "Long",
+})
