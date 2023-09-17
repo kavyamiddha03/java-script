7e4cb6712dd7e9be8f0e5a2b71f8936baeac470f
kavyamiddha03 <143822115+kavyamiddha03@users.noreply.github.com>
2023-09-17 09:04:36 +0000

    json

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,29 @@
+const course = {
+    courseName: "js in hindi",
+    price: "999",
+    instructor: "hitesh" 
+}
+
+// console.log(course.instructor);
+
+
+// object de structure
+const {instructor: teacher} = course;
+console.log(instructor);
+
+// JSON API
+// Syntax
+// Api = randomUserme api
+// to read api = json formatter
+
+{
+    "name": "hitesh",
+    "price": "free",
+    "channel": "chai aur code"
+}
+
+[
+    {},
+    {},
+    {}
+]
